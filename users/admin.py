e9a185a61e4c73cb0b231aa3858ccd094a91c4ea
keki <baejaehwan1125@gmail.com>
2021-10-01 16:20:58 +0900

    test

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.forms import Textarea
 
 class UserAdminConfig(UserAdmin):
     """
@@ -8,7 +9,27 @@ class UserAdminConfig(UserAdmin):
     ORDER ITEMS BY RECENT START DATE
     AND SHOWS EMAIL, USERNAME AND STATUS
     """
+    model = User
+    search_fields = ('email', 'username')
+    list_filter = ('email', 'username', 'is_active', 'is_staff')
     ordering = ('-start_date',)
     list_display = ('email', 'username', 'is_active', 'is_staff')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'username',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')},),
+        ('Personnal', {'fields': ('about',)})
+        )
+    
+    formfield_overrides = {
+        User.about: {'widget': Textarea(attrs={'rows':10, 'cols':40})}
+    }
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_active', 'is_staff')}
+        ),
+    )
 
 admin.site.register(User, UserAdminConfig)
